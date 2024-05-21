a6033f787b90899d1b161f9edb5f8e715a026beb
Meshu <code@labs.meshu.us>
2024-05-21 12:09:04 -0400

    Create ScPackageEstimateReport.ps1

---
+++ ScPackageEstimateReport.ps1
@@ -0,0 +1,22 @@
+﻿
+$PackageMediaRoot = Get-Item "master:/media library"
+$MediaItems = $PackageMediaRoot.Axes.GetDescendants() | Where-Object {[int]$_.Fields["Size"].Value -gt 0} | Initialize-Item
+
+[int]$PackageMediaSizeEstimate = 0
+
+foreach($MediaItem in $MediaItems) {
+
+    $PackageMediaSizeEstimate += [double]$MediaItem["Size"]/1024
+}
+
+$PackageMediaReportProps = @{
+    InfoTitle = "Package Contents"
+    InfoDescription = "Estimated Package Size: $_($PackageMediaSizeEstimate MB)"
+    PageSize = 20
+}
+
+
+$MediaItems |
+    Show-ListView @PackageMediaReportProps -Property @{Label="Name"; Expression={$_.DisplayName}},
+        @{Label="Size %"; Expression={[Math]::Round(([double]$_.Size/1024)/$PackageMediaSizeEstimate, 3)}},
+        @{Label="Path"; Expression={$_.ItemPath}}
